00047268266b3478fe6e1e0e25d4486cd2d176dc
theperrinpages <60055497+theperrinpages@users.noreply.github.com>
2020-03-15 05:54:39 +1100

    Add files via upload

---
+++ email sender portable/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace email_sender_portable
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.chkCaseID = new System.Windows.Forms.CheckBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtSubject = new System.Windows.Forms.TextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtSender = new System.Windows.Forms.TextBox();
+            this.txtReceiver = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblSent = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Location = new System.Drawing.Point(105, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Sender:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(105, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Receiver:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Location = new System.Drawing.Point(105, 237);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Subject";
+            // 
+            // chkCaseID
+            // 
+            this.chkCaseID.AutoSize = true;
+            this.chkCaseID.BackColor = System.Drawing.Color.Transparent;
+            this.chkCaseID.Checked = true;
+            this.chkCaseID.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkCaseID.Location = new System.Drawing.Point(643, 211);
+            this.chkCaseID.Name = "chkCaseID";
+            this.chkCaseID.Size = new System.Drawing.Size(197, 29);
+            this.chkCaseID.TabIndex = 5;
+            this.chkCaseID.Text = " Include case ID";
+            this.chkCaseID.UseVisualStyleBackColor = false;
+            // 
+            // btnSend
+            // 
+            this.btnSend.BackColor = System.Drawing.Color.DimGray;
+            this.btnSend.Location = new System.Drawing.Point(117, 528);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(122, 47);
+            this.btnSend.TabIndex = 6;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = false;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // txtSubject
+            // 
+            this.txtSubject.Location = new System.Drawing.Point(225, 237);
+            this.txtSubject.Name = "txtSubject";
+            this.txtSubject.Size = new System.Drawing.Size(359, 31);
+            this.txtSubject.TabIndex = 7;
+            this.txtSubject.Text = "smtp client sql query database..";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(228, 299);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(377, 127);
+            this.txtMessage.TabIndex = 9;
+            this.txtMessage.Text = "This message was sent from smtp client. This program was written in c# using Visu" +
+    "al Studio 2019.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.Transparent;
+            this.label4.Location = new System.Drawing.Point(108, 299);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Message";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.linkLabel1.Location = new System.Drawing.Point(820, 610);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(205, 25);
+            this.linkLabel1.TabIndex = 10;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "http://kernelspy.com";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(771, 68);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(281, 31);
+            this.txtPassword.TabIndex = 12;
+            this.txtPassword.Text = "password";
+            this.txtPassword.UseSystemPasswordChar = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.Transparent;
+            this.label5.Location = new System.Drawing.Point(651, 68);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 25);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Password";
+            // 
+            // txtSender
+            // 
+            this.txtSender.Location = new System.Drawing.Point(225, 68);
+            this.txtSender.Name = "txtSender";
+            this.txtSender.Size = new System.Drawing.Size(235, 31);
+            this.txtSender.TabIndex = 14;
+            this.txtSender.Text = "cybray.it";
+            // 
+            // txtReceiver
+            // 
+            this.txtReceiver.Location = new System.Drawing.Point(225, 142);
+            this.txtReceiver.Name = "txtReceiver";
+            this.txtReceiver.Size = new System.Drawing.Size(235, 31);
+            this.txtReceiver.TabIndex = 15;
+            this.txtReceiver.Text = "cybray.it";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.Transparent;
+            this.label6.Location = new System.Drawing.Point(466, 71);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(130, 25);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "@gmail.com";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.BackColor = System.Drawing.Color.Transparent;
+            this.label7.Location = new System.Drawing.Point(466, 148);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(130, 25);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "@gmail.com";
+            // 
+            // lblSent
+            // 
+            this.lblSent.AutoSize = true;
+            this.lblSent.BackColor = System.Drawing.Color.Transparent;
+            this.lblSent.Location = new System.Drawing.Point(313, 539);
+            this.lblSent.Name = "lblSent";
+            this.lblSent.Size = new System.Drawing.Size(143, 25);
+            this.lblSent.TabIndex = 18;
+            this.lblSent.Text = "Transmitted...";
+            this.lblSent.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(1175, 656);
+            this.Controls.Add(this.lblSent);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtReceiver);
+            this.Controls.Add(this.txtSender);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtSubject);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.chkCaseID);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.Color.PaleGreen;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Opacity = 0.95D;
+            this.Text = "Email sender";
+            this.TransparencyKey = System.Drawing.Color.Gray;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox chkCaseID;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtSubject;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtSender;
+        private System.Windows.Forms.TextBox txtReceiver;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblSent;
+    }
+}
+
---
+++ email sender portable/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Net.Mail;
+using System.Net;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace email_sender_portable
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string s = txtSender.Text + "@gmail.com";
+            string r = txtReceiver.Text + "@gmail.com";
+
+            lblSent.Text = "Transmitting...";
+            lblSent.Visible = true;
+           
+            try
+            {
+                SmtpClient smtp = new SmtpClient("smtp.gmail.com");
+                MailMessage mail = new MailMessage();
+
+                smtp.Port = 587;
+                smtp.Credentials = new NetworkCredential(s, txtPassword.Text);
+                smtp.EnableSsl = true;
+
+                mail.From = new MailAddress(s);
+                mail.To.Add(r);
+                mail.Subject = txtSubject.Text;
+                mail.Body = txtMessage.Text;
+
+
+                if (chkCaseID.Checked)
+                {
+                    mail.Subject += "Case ID: " + rand().ToString();
+                }
+
+                smtp.Send(mail);
+
+                lblSent.Text = "Transmitted...";
+
+           
+
+
+
+
+
+
+
+
+
+
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "OOps.. error encountered...");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        int rand()
+        {
+
+            Random number = new Random();
+            int n = number.Next();
+            return n;
+        }
+    }
+}
